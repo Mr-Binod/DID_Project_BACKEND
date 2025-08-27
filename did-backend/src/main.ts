923ca888a8ffd74a21a5e127327008a71c91b365
Mr-Binod <blackorion3@gmail.com>
2025-08-27 14:17:28 +0900

    [fix] multer

--- did-backend/src/main.ts
+++ did-backend/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
 	  credentials : true
 	 }))
   app.use('/uploads', express.static(join(__dirname, '..','..', 'uploads')));
-  app.useGlobalPipes(new ValidationPipe());
+  // app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
